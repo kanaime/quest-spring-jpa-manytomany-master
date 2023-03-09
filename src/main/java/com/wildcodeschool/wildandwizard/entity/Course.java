c0e8ef6ff7ed5ad095bbcd96f17056f6c41a6c4b
lksAnton <lukusaanton@gmail.com>
2023-03-09 10:08:58 +0100

    comit

---
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -0,0 +1,58 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
+
+    public Course() {
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Wizard> getWizards() {
+        return wizards;
+    }
+
+    public void setWizards(List<Wizard> wizards) {
+        this.wizards = wizards;
+    }
+
+}
---
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -0,0 +1,106 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.*;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "wizard")
+public class Wizard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "birthday")
+    private Date birthday;
+    @Column(name = "birth_place")
+    private String birthPlace;
+    @Column(name = "biography")
+    private String biography;
+    @Column(name = "muggle")
+    private boolean muggle;
+    @ManyToMany
+    @JoinTable(name = "wizard_course", joinColumns = @JoinColumn(name = "wizard_id"), inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private List<Course> courses = new ArrayList<>();
+
+    public Wizard() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public boolean isMuggle() {
+        return muggle;
+    }
+
+    public void setMuggle(boolean muggle) {
+        this.muggle = muggle;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+}
